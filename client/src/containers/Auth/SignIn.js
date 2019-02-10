b2b58f45c85cdb4fe6f24db83ee1fef5145b909c
Hye Soo Park <flyjwayur@gmail.com>
2019-02-10 08:30:55 +0200

    Add breakpoints for md/lg size in signUp/In grid

--- client/src/containers/Auth/SignIn.js
+++ client/src/containers/Auth/SignIn.js
@@ -14,12 +14,6 @@ import auth from '../../authentication/auth';
 import { Redirect } from 'react-router-dom';
 
 const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    flexGrow: 1,
-  },
   signIn__form_container: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -125,8 +119,8 @@ class SignIn extends Component {
     }
 
     return (
-      <Grid container className={classes.container}>
-        <Grid item xs={12} sm={6}>
+      <Grid container justify='center'>
+        <Grid item xs={12} sm={6} md={6} lg={4}>
           <Paper>
             <div className={classes.signIn__form_title}>
               <Typography variant='h5' color='textSecondary' gutterBottom>
